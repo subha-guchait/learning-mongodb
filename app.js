88fa36eab6f7e177981ac7060305c1ce9f0bd6a1
Subhankar <guchaitsubhankar0@gmail.com>
2025-07-11 19:05:22 +0530

    add cart in user table ad implemented add to cart feature

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("6870e2b4b01926bf02754d64")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
